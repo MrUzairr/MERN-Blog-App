c6603b40775b147a57f9075114c002b1042124bc
Muhammad Uzair <105927747+MrUzairr@users.noreply.github.com>
2023-07-30 14:26:05 +0500

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.set('view engine', 'jade');
 
 app.use(cors());
 app.use((req,res,next)=>{
-  res.setHeader('Access-Control-Allow-Origin','http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin','https://mern-blogs-app.netlify.app/');
   res.setHeader('Access-Control-Allow-Methods','GET','POST','PUT','DELETE');
   res.setHeader('Access-Control-Allow-Headers','Content-Types');
   next();
